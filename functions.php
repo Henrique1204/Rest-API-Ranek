dd7d3d9dd14b12a9d5d066779093516a1774d605
Paulo <pauloh16.pdr@gmail.com>
2021-02-16 14:57:20 -0300

    Criar endpoint para get de produto

--- functions.php
+++ functions.php
@@ -10,6 +10,21 @@ require_once($DIR . '/endpoints/usuario_post.php');
 require_once($DIR . '/endpoints/usuario_get.php');
 require_once($DIR . '/endpoints/usuario_put.php');
 require_once($DIR . '/endpoints/produto_post.php');
+require_once($DIR . '/endpoints/produto_get.php');
+
+function get_produto_id_by_slug($slug) {
+    $args = array(
+        'name' => $slug,
+        'post_type' => 'produto',
+        'numberposts' => 1,
+        'fields' => 'ids'
+    );
+
+    $query = new WP_Query($args);
+    $posts = $query->get_posts();
+
+    return array_shift($posts);
+}
 
 function expirar_token() {
     return time() + (60 * 60 * 24);
