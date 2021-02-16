0a005bb827a451ab3ae9ff5bf3bab658f3e59b00
Paulo <pauloh16.pdr@gmail.com>
2021-02-16 16:43:20 -0300

    Criar endpoint para get de lista de produtos

--- endpoints/produto_get.php
+++ endpoints/produto_get.php
@@ -1,7 +1,6 @@
 <?php
 
-function api_produto_get($req) {
-    $slug = $req['slug'];
+function produto_scheme($slug) {
     $post_id = get_produto_id_by_slug($slug);   
 
     if ($post_id) {
@@ -34,7 +33,11 @@ function api_produto_get($req) {
         $res = new WP_Error('naoexiste', 'Produto não encontrado.', array( 'status' => 404 ));
     }
 
-    return rest_ensure_response($res);
+    return $res;
+}
+
+function api_produto_get($req) {
+    return rest_ensure_response(produto_scheme($req['slug']));
 }
 
 function registrar_api_produto_get() {
@@ -47,3 +50,63 @@ function registrar_api_produto_get() {
 }
 
 add_action('rest_api_init', 'registrar_api_produto_get');
+
+function api_produtos_get($req) {
+    $q = sanitize_text_field($req['q']) ?: '';
+    $_page = sanitize_text_field($req['_page']) ?: 0;
+    $_limit = sanitize_text_field($req['_limit']) ?: 9;
+    $usuario_id = sanitize_text_field($req['usuario_id']);
+
+    $usuario_id_query = null;
+
+    if ($usuario_id) {
+        $usuario_id_query = array(
+            'key' => 'usuario_id',
+            'value' => $usuario_id,
+            'compare' => '='
+        );
+    }
+
+    $vendido = array(
+        'key' => 'vendido',
+        'value' => 'false',
+        'compare' => '='
+    );
+
+    $query = array(
+        'post_type' => 'produto',
+        'posts_per_page' => $_limit,
+        'paged' => $_page,
+        's' => $q,
+        'meta_query' => array(
+            $usuario_id_query,
+            $vendido
+        )
+    );
+
+    $loop = new WP_Query($query);
+    $posts = $loop->posts;
+    $total = $loop->found_posts;
+
+    $produtos = array();
+
+    foreach ($posts as $key => $value) {
+        $produtos[] = produto_scheme($value->post_name);
+    }
+
+    $res = rest_ensure_response($produtos);
+    $res->header('X-Total-Count', $total);
+
+    return $res;
+}
+
+function registrar_api_produtos_get() {
+    $args = array(
+        'methods' => WP_REST_Server::READABLE,
+        'callback' => 'api_produtos_get'
+    );
+
+    register_rest_route('api', '/produto', array($args));
+}
+
+add_action('rest_api_init', 'registrar_api_produtos_get');
